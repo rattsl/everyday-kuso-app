9f2a7b6de493dd98eefc9c6678a3e8d1f5984839
rattsl <r4ttsl@gmail.com>
2021-12-07 18:02:44 +0900

    error: 環境変数が取れない

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,28 @@
 import { useState } from "react";
 
+// type _Config = {
+//   API_KEY: string;
+// }
+
+// const Config: _Config = {
+//   API_KEY: process.env.REACT_APP_API_URL || ""
+// }
+
 const Form = () => {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const apiURL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London&aqi=no`;
+    fetch(apiURL).then(res => res.json()).then(data => console.log(data));
+  };
   return (
     <form >
       <input type="text" name="city" onChange={e => setCity(e.target.value)}/>
-      {city}
-      <button>Get Weather</button>
+      <button type="submit" onClick={getWeather} >Get Weather</button>
+      <div>{process.env.REACT_APP_API_KEY}</div>
     </form>
   )
-}
+};
 
 export default Form;
