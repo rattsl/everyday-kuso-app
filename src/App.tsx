46096d17b35ec40a86683803b66e7cbbfebdff05
rattsl <r4ttsl@gmail.com>
2021-12-08 22:30:02 +0900

    implement: inputの値をstateに登録して値が変わる度にレンダリングするよう実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState("");
   return (
     <div className="App">
-      <h1>ヘロウワールド</h1>
+      <h1>{text}</h1>
+      <input type="text" onChange={e => setText(e.target.value)} />
     </div>
   );
 }
