3978d6766c43e82dc8c1a8e934c4eecf118c43f8
rattsl <r4ttsl@gmail.com>
2021-12-19 14:37:11 +0900

    add: httpcats実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [status, setStatus] = useState("");
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStatus(e.target.value)
+  }
+  const srcUrl: string = `https://http.cat/${status}`;
   return (
     <div className="App">
-      <h1>ヘロウワールド</h1>
+      <img src={srcUrl} alt="" />
+      <form onSubmit={e => e.preventDefault()}>
+        <input type="text" onChange={e => handleOnChange(e)}/>
+      </form>
     </div>
   );
 }
