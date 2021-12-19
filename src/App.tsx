94ac32e683093f312b44ab0dd9fd7f7f98cbf4b9
rattsl <r4ttsl@gmail.com>
2021-12-19 14:45:05 +0900

    add: style実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const imageStyle = {
+    width: "100%"
+  }
   const [status, setStatus] = useState("");
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setStatus(e.target.value)
@@ -9,7 +12,7 @@ function App() {
   const srcUrl: string = `https://http.cat/${status}`;
   return (
     <div className="App">
-      <img src={srcUrl} alt="" />
+      <img src={srcUrl} alt="" style={imageStyle}/>
       <form onSubmit={e => e.preventDefault()}>
         <input type="text" onChange={e => handleOnChange(e)}/>
       </form>
