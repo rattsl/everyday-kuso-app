2ce0ca1a8f5a9eeed13134936a49dd6958debf7c
rattsl <r4ttsl@gmail.com>
2022-01-09 08:31:00 +0900

    add: カスタムフックを使わないでdataをfetchして表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+// データフェッチ中はローディングを表示
+// 成功したらデータ表示、失敗したらエラー表示をする
+// ローディングの情報を持ったstateをもつ
+// ローディング中はフラグtrue,終わったらfalseにする
+
+
+
 import axios from 'axios';
 import { useState } from 'react';
 import './App.css';
@@ -5,16 +12,13 @@ import { UserCard } from './components/UserCard';
 import { User } from './types/api/user';
 import { UserProfile } from './types/userProfile';
 
-const user = {
-  id: 1,
-  name: "hogehoge",
-  email: "hoge@hoge.com",
-  address: "hogeaddress"
-}
-
 function App() {
   const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
   const onClickFetchUser = () => {
+    setLoading(true);
+    setError(false);
     axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res) => {
       const data = res.data.map((user) => ({
         id: user.id,
@@ -22,16 +26,26 @@ function App() {
         email: user.email,
         address: `${user.address.city}${user.address.suite}${user.address.street}`
       }))
-      setUserProfiles(data)
+      setUserProfiles(data);
+    }).catch(() => {
+      setError(true)
+    }).finally(() => {
+      setLoading(false);
     })
   }
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
-      {userProfiles.map((user) => (
-        <UserCard key={user.id} user={user}/>
-      ))}
-      <UserCard user={user} />
+      {error ? (
+        <p>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>loading...</p>
+      ) : 
+      <>
+        {userProfiles.map((user) => (
+          <UserCard key={user.id} user={user}/>
+        ))}
+      </>}
     </div>
   );
 }
