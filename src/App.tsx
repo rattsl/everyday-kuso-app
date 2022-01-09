c9cc185030fc3c2069bc74a8c1a8fdd830ed8b71
rattsl <r4ttsl@gmail.com>
2022-01-09 09:13:33 +0900

    add: カスタムフックから呼び出し

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,13 @@
 // ローディング中はフラグtrue,終わったらfalseにする
 
 
-
-import axios from 'axios';
-import { useState } from 'react';
 import './App.css';
 import { UserCard } from './components/UserCard';
-import { User } from './types/api/user';
-import { UserProfile } from './types/userProfile';
+import { useAllUsers } from "./hooks/useAllUsers";
 
 function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<boolean>(false);
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res) => {
-      const data = res.data.map((user) => ({
-        id: user.id,
-        name: `${user.name}${user.username}`,
-        email: user.email,
-        address: `${user.address.city}${user.address.suite}${user.address.street}`
-      }))
-      setUserProfiles(data);
-    }).catch(() => {
-      setError(true)
-    }).finally(() => {
-      setLoading(false);
-    })
-  }
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchUser = () => getUsers();
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
